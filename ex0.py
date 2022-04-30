07812b91f31529bf9d3aad232e6d73b1c26bee67
Or Shahar <orshahar91@gmail.com>
2022-05-01 01:35:59 +0300

    ex0 Code review

--- ex0.py
+++ ex0.py
@@ -1,19 +1,22 @@
 from PIL import Image
 
 
-def decipher(image_path):
-    #  decipher the hidden message inside the image
-    black_pixels = []
+def decipher(image):
+    """
+    Decipher the hidden message inside the image.
+    :param image: The path for the image file.
+    :return: The coded message as string.
+    """
     coded_message = ""
     threshold = 127  # lower than this is black
-    image = Image.open(image_path)
     pixel_matrix = image.load()
     width, height = image.size
-    for col in range(width):
-        for row in range(height):
-            if pixel_matrix[col, row] < threshold:
-                black_pixels.append(row)
-
-    for index_row in black_pixels:
-        coded_message += chr(index_row)
+    black_pixels = [(col, row) for col in range(width) for row in range(height) if pixel_matrix[col, row] < threshold]
+    coded_message = [coded_message + chr(index[1]) for index in black_pixels]
+    coded_message = ''.join(coded_message)
     return coded_message
+
+
+"""Driver code to test the function above."""
+img_to_decipher = Image.open("code.png")
+print(decipher(img_to_decipher))
